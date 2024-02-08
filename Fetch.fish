7a4827a9cfc75e93391d539b334c5be7c65587a4
Gruficlath <glitchycrafting@gmail.com>
2024-02-07 22:40:12 -0600

    Be more specific in gitignore and add more to fetch script

--- Fetch.fish
+++ Fetch.fish
@@ -3,3 +3,11 @@
 for f in $(/bin/ls XDGConfigs)
 	cp -vr $HOME/.config/$f ./XDGConfigs/
 end
+
+for f in $(/bin/ls -a HomeConfigs)
+	cp -vr $HOME/$f ./HomeConfigs/
+end
+
+for f in $(/bin/ls HomeDirs)
+	cp -vr $HOME/$f ./HomeDirs/
+end
